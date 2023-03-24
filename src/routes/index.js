74ee986931ba056a07306a83a59e0407ca86167f
Jonathan Benitez <jonathan92_24@hotmail.com>
2023-03-24 00:58:30 -0300

    actualizado con developer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-// const productRouter = require("./productRouter");
+const productRouter = require("./productRouter");
 const userRouter = require("./userRouters");
 
 const router = Router();
 
-// router.use('/product', productRouter);
+router.use('/product', productRouter);
 router.use('/user', userRouter);
 
 module.exports = router;
