51a87bf2dbeffd08715c0c17495f7a8d089b1350
Jonathan Benitez <jonathan92_24@hotmail.com>
2023-03-25 05:03:58 -0300

    rutas get y post de Newsletter terminadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ const productRouter = require("./productRouters");
 const userRouter = require("./userRouters");
 const reviewsRouter = require("./reviewsRouter");
 const filterRouter = require("./arrFilterRouters");
+const newsletterRouter = require("./newsletterRouter");
 
 const router = Router();
 
 router.use('/product', productRouter);
 router.use('/user', userRouter);
 router.use('/reviews', reviewsRouter);
-router.use('/filter', filterRouter)
+router.use('/filter', filterRouter);
+router.use("/newsletter", newsletterRouter)
 
 module.exports = router;
