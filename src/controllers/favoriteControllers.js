affc13ae2eaa52cae3c19a6a0cafef2ec205b0d3
Jonathan Benitez <jonathan92_24@hotmail.com>
2023-03-26 16:52:14 -0300

    terminada funciones para borrar un favorito o toods los favoritos

--- src/controllers/favoriteControllers.js
+++ src/controllers/favoriteControllers.js
@@ -3,6 +3,7 @@ const {
     LoginUser
 } = require("../db")
 
+//Para agragar los favoritos del usuario con el metodo addProduct() que se crea de la relacion de las tablas
 const guardarFavorite = async (userId, productId) => {
     const user = await LoginUser.findByPk(userId);
     const product = await Product.findByPk(productId);
@@ -10,14 +11,33 @@ const guardarFavorite = async (userId, productId) => {
     return product;
 }
 
+//Para traer los favoritos del usuario con el metodo getProducts() que se crea de la relacion de las tablas
 const getFavoritos = async (userId) => {
     const user = await LoginUser.findByPk(userId);
     console.log(userId);
     const favoritos = await user.getProducts();
     return favoritos
 }
+//para remover un usuario, se usa el metodo removeProduct() que se crea de la relacion de las tablas
+const deleteFavorito = async (userId, productId) => {
+    const user = await LoginUser.findByPk(userId);
+    const product = await Product.findByPk(productId);
+    await user.removeProduct(product);
+    return product;
+  }
+
+//Para eliminar todos los favoritos del usuario setiando la tabla de nuevo a un array vacio setProducts([])
+const vaciarFavoritos = async (userId) => {
+    console.log(userId)
+    const user = await LoginUser.findByPk(userId);
+    const product = []
+    await user.setProducts(product);
+    return user
+  }
 
 module.exports = {
     guardarFavorite,
-    getFavoritos
+    getFavoritos,
+    deleteFavorito,
+    vaciarFavoritos
 }
