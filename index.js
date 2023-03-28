8fe0a72357ddb01d1c91597eabe926867afe84f8
JEBenitez92 <108236690+JEBenitez92@users.noreply.github.com>
2023-03-28 20:56:36 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app.js");
-const { sequelize } = require("./src/db.js");
-const port = 3001
-
-app.listen(port,()=>{
-    sequelize.sync({ force: true });
-    console.log(`Server levantado en puerto ${port}`)
-})
