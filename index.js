db283eb065336537446763938921768764186835
id="Eugenio Aragón" / <97991009+euaragon@users.noreply.github.com>
2023-04-03 18:37:57 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app.js");
-const { sequelize } = require("./src/db.js");
-const port = 3001
-
-app.listen(port,()=>{
-    sequelize.sync({ force: false });
-    console.log(`Server levantado en puerto ${port}`)
-})
