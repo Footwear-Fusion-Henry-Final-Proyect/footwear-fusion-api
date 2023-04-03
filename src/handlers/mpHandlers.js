6a941f914264672142ec079fa24a7e475712a3fe
JEBenitez92 <108236690+JEBenitez92@users.noreply.github.com>
2023-04-03 17:36:36 -0300

    a la fecha

--- src/handlers/mpHandlers.js
+++ src/handlers/mpHandlers.js
@@ -9,24 +9,38 @@ mercadopago.configure({
 });
 
 const createPreferenceHandlers = async (req, res) => {
+    const item = req.body.item
+    const usuario = req.body.user
+    console.log(req.body);
+    console.log(usuario);
     let preference = {
-        items: [
-            {
-                title: req.body.name,
-                unit_price: parseInt(req.body.price),
-                quantity: parseInt(req.body.quantity),
-            }
-        ],
-        // payer: {
-        //     name: user.name,
-        //     email: user.email
-        // },
+        items: item,
+        payer: {
+			phone: {
+				area_code: req.body.user.area_code,
+				number: parseInt(req.body.user.number),
+			},
+			// address: {
+			// 	zip_code: req.body.zip_code,
+			// 	street_name: req.body.street_name,
+			// 	street_number: parseInt(req.body.street_number)
+			// },
+			// email: req.body.email,
+			// identification: {
+			// 	number: req.body.number,
+			// 	type: req.body.type
+			// },
+			// name: req.body.name,
+			// surname: req.body.surname,
+		},
+		
         back_urls: {
-            "success": "http://localhost:3001/mp/feedback",
-            "failure": "http://localhost:3001/mp/feedback",
-            "pending": "http://localhost:3001/mp/feedback"
+            "success": "http://localhost:3000",
+            "failure": "http://localhost:3000",
+            "pending": ""
         },
         auto_return: "approved",
+        binary_mode: true,
     };
 
     mercadopago.preferences.create(preference)
