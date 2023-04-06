ec5909ff4c4fc1344aa13c07bc19fe5dc117a108
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-04-06 11:25:20 -0300

    newsletter

--- src/middlewares/userValidator.js
+++ src/middlewares/userValidator.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const {LoginUser, Role} = require("../db");
+const { LoginUser, Role } = require("../db");
 const { SECRET } = process.env;
 
 const verifyToken = async (req, res, next) => {
@@ -10,15 +10,14 @@ const verifyToken = async (req, res, next) => {
     if (!token) return res.status(401).json({ message: "no token provided" }); //si el usuario no envia x-access-token, error
 
     const tokenUser = jwt.verify(token, SECRET); //extraigo lo que esta dentro del token
-    
+
     req.userId = tokenUser.id;
 
     const user = await LoginUser.findByPk(req.userId); //no preciso la contraseña
-    
+
     if (!user) return res.status(404), json({ message: "user not found" });
 
     next();
-
   } catch (error) {
     return res.status(404).json({ message: "unauthorized" });
   }
@@ -26,16 +25,16 @@ const verifyToken = async (req, res, next) => {
 
 const isAdmin = async (req, res, next) => {
   try {
-    
     const user = await LoginUser.findByPk(req.userId);
-    
+
     if (!user) return res.status(404).json({ message: "user not found" });
 
     const UserRol = await Role.findByPk(user.RoleId); // busco el rol del usuario
-   
-    if (UserRol.Rol !== "admin") return res.status(401).json({ message: "administrator role required" });
-    console.log("rol",UserRol.Rol);
-      
+
+    if (UserRol.Rol !== "admin")
+      return res.status(401).json({ message: "administrator role required" });
+    console.log("rol", UserRol.Rol);
+
     next();
   } catch (error) {
     return res.status(500).json({ message: "error" });
@@ -43,6 +42,6 @@ const isAdmin = async (req, res, next) => {
 };
 
 module.exports = {
-    verifyToken,
-    isAdmin
-}
+  verifyToken,
+  isAdmin,
+};
